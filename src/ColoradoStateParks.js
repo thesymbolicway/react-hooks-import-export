03954b4f930872e88443e79e503d1bb1e3493302
daniel.arisa@yahoo.com <daaniel.arisa@yahoo.com>
2022-08-28 21:43:53 -0500

    commit tree eb8f6301ab18d1de7857ea9df94931268d321d01

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,24 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
+import {
+  trees as parkTrees,
+  wildlife as parkWildlife,
+} from "./parks/RockyMountain";
 
-function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+console.log(RMFunctions.trees);
+RMFunctions.wildlife();
+
+
+console.log(parkTrees);
+parkWildlife();
+
+export default function ColoradoStateParks() {
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  );
 }
